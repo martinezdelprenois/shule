b2309d8ec3e574bd2add9e6f43e3ed93f8eac5a3
martinezdelprenois <martintumwine@gmail.com>
2017-02-08 17:08:37 +0300

    a few more changes

--- app/src/main/java/com/example/marto/my_school_revierwer_4/Home_two.java
+++ app/src/main/java/com/example/marto/my_school_revierwer_4/Home_two.java
@@ -2,6 +2,8 @@ package com.example.marto.my_school_revierwer_4;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * Created by marto on 07-Feb-17.
@@ -9,10 +11,17 @@ import android.os.Bundle;
 
 public class Home_two extends Activity {
 
+    Spinner spin_category, spin_education_level;
+    ArrayAdapter<CharSequence> array_category, array_education_level;
+
+
+
     @Override
     protected void onCreate(Bundle state){
         super.onCreate(state);
         setContentView(R.layout.home_two);
 
     }
+
+    private void
 }
